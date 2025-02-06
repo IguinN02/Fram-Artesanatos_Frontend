fee0b8b5168f047189071ae2e99f245c9cc2b33e
Igor <igoroliveira0150@gmail.com>
2025-02-06 20:45:59 -0300

    Rota de cadastro/login adicionada

--- fram-artesanato/src/components/Header.js
+++ fram-artesanato/src/components/Header.js
@@ -175,14 +175,22 @@ const Header = () => {
               {searchText.trim() === "" && (
                 <>
                   <li className="nav-lista__item">
-                    <Link to="/TodosProdutos" onClick={() => setIsMenuOpen(false)}>
+                    <Link to="/Cadastro" onClick={() => setIsMenuOpen(false)}>
                       <div className="box_conta">
                         <svg
                           xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                         </svg>
-                        <p className="test_pesq conta_titulo">Criar Conta/Login</p>
+                        <p className="test_pesq conta_titulo">Criar Conta</p>
+                      </div>
+                    </Link>
+                  </li>
+
+                  <li className="nav-lista__item">
+                    <Link to="/Login" onClick={() => setIsMenuOpen(false)}>
+                      <div className="box_conta">
+                        <p className="test_pesq conta_titulo">Login</p>
                       </div>
                     </Link>
                   </li>
