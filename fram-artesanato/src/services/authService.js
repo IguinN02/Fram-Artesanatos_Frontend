47abed997161bf5f77b67b41253b4ace73a7a215
Igor <igoroliveira0150@gmail.com>
2025-02-11 20:18:47 -0300

    Volta

--- fram-artesanato/src/services/authService.js
+++ fram-artesanato/src/services/authService.js
@@ -4,6 +4,7 @@ export const cadastrarUsuario = async (dados) => {
   const resposta = await fetch(`${API_URL}/cadastro`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
+    credentials: "include",
     body: JSON.stringify(dados),
   });
   return resposta.json();
@@ -19,7 +20,7 @@ export const logarUsuario = async (dados) => {
 };
 
 export const buscarPerfil = async (token) => {
-  const resposta = await fetch(`${API_URL}/perfil`, {
+  const resposta = await fetch(`${API_URL}/Perfil`, {
     method: "GET",
     mode: 'cors',
     headers: {
